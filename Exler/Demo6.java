d058accdf1d739157691c987f91680ff366a7c50
Vajrala Yashwanth Reddy <yashwanthvajrala7995@gmail.com>
2025-01-03 10:09:05 +0530

    Add files via upload
    While Loop

---
+++ Exler/Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+        public static void main (String[] args){
+            String username = "Yashwanth";
+            String pwd = "Yashwanth@123";
+            while(username == "Yashwanth" && pwd == "Yashwanth@123"){
+                System.out.println("Hello yashwanth Happy New Year!");
+                break;
+            }
+            }
+        }
