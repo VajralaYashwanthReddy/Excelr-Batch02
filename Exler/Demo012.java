140846d6a5845da819664a50655c9fa9356ae98e
Vajrala Yashwanth Reddy <yashwanthvajrala7995@gmail.com>
2025-01-26 18:19:19 +0530

    Area of the circle
    Area of the circle

--- Exler/Demo012.java
+++ Exler/Demo012.java
@@ -1,7 +1,6 @@
+import java.util.Scanner;
 
-    import java.util.Scanner;
-
-public class Demo12 {
+public class Demo012 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
