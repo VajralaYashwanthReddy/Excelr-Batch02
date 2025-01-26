732ccaaf92d9e301553b2ddc79ad513914015581
Vajrala Yashwanth Reddy <yashwanthvajrala7995@gmail.com>
2025-01-26 18:08:59 +0530

    Username and Password
    Username and Password

---
+++ Exler/Demo006.java
@@ -0,0 +1,11 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String name="Yashwanth";
+        String password="Yashwanth@123";
+        while((name=="Yashwanth")&&(password=="Yashwanth@123")){
+            System.out.println("hello Have A Nice Day");
+            break;
+    }
+    
+}
+}
