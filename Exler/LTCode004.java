37e57c157242a942e4f08fb23b2e443e2d640dbe
Vajrala Yashwanth Reddy <yashwanthvajrala7995@gmail.com>
2025-01-26 18:44:13 +0530

    add two numbers_reverse order using LeetCode
    add two numbers_reverse order using LeetCode

--- Exler/LTCode004.java
+++ Exler/LTCode004.java
@@ -15,7 +15,6 @@ public class LTCode004 {
             return dummyHead.next;
         }
     }
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, current = dummyHead;
